f3c57ed5d619b117f2eabefb1adcfa751b7ea9b5
nobewan <inobeandre@gmail.com>
2026-02-13 20:24:39 -0800

    added stuff

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,7 +3,7 @@ function Dashboard(){
     <div className = "h-16 items-center flex justify-between">
       <div className =" text-white p-4">
         <h1 className = "text-2xl font-bold">Pantry Pal</h1>
-        <p>Camera tracker for food expiration</p>
+        <p>Camera tracker for food</p>
         </div>
       
     </div>
