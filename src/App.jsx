09e635b038eb998537c8c34b306f0c39b9ae3527
nobewan <inobeandre@gmail.com>
2026-02-13 20:21:00 -0800

    Added stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import Dashboard from './components/Dashboard.jsx'
 import Cards from './components/Cards.jsx'
+import Camera from './components/Camera.jsx'
+import Result from './components/Result.jsx'
 import WebcamCapture from './components/WebcamCapture.jsx'
+import Notifications from './components/Notifications.jsx'
+import FoodMonitor from './components/FoodMonitor.jsx'
 
 function App() {
     return(
         <>
+        <body className = "bg-slate-300"></body>
         <Dashboard></Dashboard>
         <Cards></Cards>
-        <WebcamCapture></WebcamCapture>
+        <Camera></Camera> {/* Placeholder for Cam */}
+        {/*<WebcamCapture></WebcamCapture>*/}
+        <Result></Result>
+        {/*<Notifications></Notifications>*/}
+        <FoodMonitor></FoodMonitor>
+
         </>
     )
   }
---
+++ src/components/Camera.jsx
@@ -0,0 +1,8 @@
+function Camera(){
+    return <div className = "bg-black m-10 h-150">
+    <h1 className = "text-white">Camera</h1>
+  </div>
+
+}
+
+export default Camera
